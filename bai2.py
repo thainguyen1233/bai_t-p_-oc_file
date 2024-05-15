7dc61ed69d80ff5afbb2bf7e55fa027504c82a07
Nguyễn Văn Thái <131363597+thainguyen1233@users.noreply.github.com>
2024-05-15 21:59:11 +0700

    Update bai2.py
    tôi sửa vào ngày hôm qua

--- bai2.py
+++ bai2.py
@@ -1,24 +1,16 @@
 def nhap_du_lieu_khach(input):
     with open(input, "r") as file:
         so_luong_khach = int(file.readline())
-        tong_trong_luong = []
-        danh_sach_khach_bi_huy = []
+        print(f"Số lượng khách: {so_luong_khach}")
+        print("Dữ liệu từng khách:")
 
         for i in range(so_luong_khach):
             trong_luong_hanh_ly = list(map(float, file.readline().split()))
+            print(f"Khách {i + 1}: {trong_luong_hanh_ly}")
+
             tong_trong_luong_hanh_ly = sum(trong_luong_hanh_ly)
-            tong_trong_luong.append(tong_trong_luong_hanh_ly)
-
             if tong_trong_luong_hanh_ly > 20 or len(trong_luong_hanh_ly) > 5:
-                danh_sach_khach_bi_huy.append(i + 1)
-
-    with open("TRONGLUONG.RA", "w") as file:
-        for trong_luong in tong_trong_luong:
-            file.write(f"{trong_luong:.1f}\n")
-
-    with open("HUYBAY.RA", "w") as file:
-        for khach in danh_sach_khach_bi_huy:
-            file.write(f"{khach}\n")
+                print(f"Khách {i + 1} bị hủy chuyến bay")
 
 if __name__ == "__main__":
     nhap_du_lieu_khach("KHACHBAY.vào")
